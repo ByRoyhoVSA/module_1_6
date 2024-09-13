416dc2320a6b8ca5c426528d012a1c7221f9cd86
ByRoyhoVSA <Forest21@inbox.ru>
2024-09-13 22:00:53 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+date_of_birth = { 'Polya' : 1999, 'max' : 2003,'Laplas' : 777 ,"black_cat" : 666}
+print(date_of_birth)
+print(date_of_birth['Laplas'])
+print(date_of_birth.get('Mr.Kukumber'))
+date_of_birth['Royho'] = 1992   #если в словаре обратится к несуществующему ключу,то он его просто создает,и добавляет к основному словарю
+date_of_birth.update({"Kolin" : 1899,"Nibelung" : -233})
+print(date_of_birth)
+del date_of_birth['Kolin']
+print(date_of_birth)
+
+my_set = {232,4432,"Kolin",'NoneKolin',9933.323,3434.555,9933.323,232,'Kolin'}
+print(my_set)
+my_set.update({'Aplle'},{(444,333)})
+my_set.discard('Aplle')
+print(my_set)
+#множества не содержат повторяющихся элементов(хранит ун. значения,разные типы(картежи,списки))
